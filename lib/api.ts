218a9a49fdb8ee5d04cd3b066c5ec07851263485
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-10 23:44:14 -0500

    api calls

---
+++ lib/api.ts
@@ -0,0 +1,95 @@
+import { Equipment } from '@/shared/api';
+import axios from 'axios';
+
+
+
+export const api = axios.create({
+  baseURL: 'http://localhost:8000', // or production URL
+  withCredentials: false,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getAllEquipmentsByUser = async (token: any) => {
+  try {
+    const response = await api.get("/api/v1/equipments", {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    if (response.status == 200) {
+      return response.data.data
+    }
+  } catch (error) {
+    throw new Error(error?.response?.data.message || 'Fetching equipments failed')
+  }
+
+}
+
+export const addNewEquipment = async (token: any, data: any) => {
+  try {
+    console.log("data: ", data)
+    const response = await api.post("/api/v1/equipments", 
+      {
+        body: data
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+
+    if (response.status == 201) {
+      return response.data.data
+    }
+  } catch (error) {
+    throw new Error(error?.response?.data.message || 'Adding equipment failed')
+  }
+}
+
+export const deleteUserEquipment = async (token:any, id: number) => {
+  try {
+    const response = await api.delete(`/api/v1/equipments/${id}`,  
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+    if (response.status == 200) {
+      return response.data.data
+    }
+  } catch (error) {
+    throw new Error(error?.response?.data.message || 'Adding equipment failed')
+  }
+}
+
+
+export const addNewWorkoutSession = async (token: any, data: any) => {
+  try {
+    console.log("data: ", data)
+    const response = await api.post("/api/v1/workouts", 
+      {
+        body: data
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+
+    if (response.status == 201) {
+      return response.data.data
+    }
+  } catch (error) {
+    throw new Error(error?.response?.data.message || 'Adding equipment failed')
+  }
+}
+
+
+
+
